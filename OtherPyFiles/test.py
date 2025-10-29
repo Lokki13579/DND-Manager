e34d50116e553340f7f3dcc8d6212282be4bec0b
Lokki13579 <l42@lokki13579.ru>
2025-10-30 01:39:54 +0300

    parsing not working

--- OtherPyFiles/test.py
+++ OtherPyFiles/test.py
@@ -1,85 +1,34 @@
-from requests_html import HTMLSession, AsyncHTMLSession
-
-session = HTMLSession()
-s2 = HTMLSession()
-asyncSession = AsyncHTMLSession()
-link = "https://dnd.su/"
+from requests_html import HTMLSession
 
 
-def searchinPage(page):
-    print(link + page)
-    return session.get(link + page)
+url = "https://dnd.su/spells/"
 
-
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) Gecko/20100101 Firefox/144.0"
+}
 spellData = {}
 
 
-def Find(req, string):
-    return req.html.find(string, first=True)
+def main(links):
+    for link in links:
+        session = HTMLSession()
+        print(link)
+        response = session.get(link)
+        response.html.render(sleep=1)
+        print(response.html.find("body", first=True).html)
+
+        spellInfo = response.html.find(".cards-wrapper", first=True)
+        print(spellInfo.text)
 
 
-def syncSearch(sLink):
-    reqs = []
-    for ln in sLink:
-        reqs.append(s2.get(ln))
-    for req in reqs:
-        req.html.render(sleep=2)
-        name = Find(req, "div.card__header > h2 > span:nth-child(1)").text
-        level, school = Find(req, ".size-type-alignment").text.split(", ")
-        castingTime = Find(req, ".params > li:nth-child(2)").text.replace(
-            "Время накладывания: ", ""
-        )
-        distance = Find(req, ".params > li:nth-child(3)").text.replace(
-            "Дистанция: ", ""
-        )
-        components = (
-            Find(req, ".params > li:nth-child(4)")
-            .text.replace("Компоненты: ", "")
-            .split(", ")
-        )
-        duration = Find(req, ".params > li:nth-child(5)").text.replace(
-            "Длительность: ", ""
-        )
-        classes = (
-            Find(req, ".params > li:nth-child(6)")
-            .text.replace("Классы: ", "")
-            .split(", ")
-        )
-        subclasses = Find(req, ".params > li:nth-child(7)").text
-        if "Подклассы" in subclasses:
-            subclasses = subclasses.replace("Подклассы: ", "").split(", ")
-        else:
-            subclasses = []
-        description = Find(req, ".params > .subsection").text
-        level = level.strip(" уровень")
-        if level not in list(spellData.keys()):
-            spellData[level] = {}
-        spellData[level][name] = {
-            "school": school,
-            "castingTime": castingTime,
-            "distance": distance,
-            "components": components,
-            "duration": duration,
-            "classes": classes,
-            "subclasses": subclasses,
-            "description": description,
-        }
+def openFile():
+    with open("OtherPyFiles/spells.txt", "r") as file:
+        return file.readlines()
 
 
-#
-# div.card__header > h2 > span:nth-child(1)
-# div.active > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > span:nth-child(1)
-# .size-type-alignment
-#
+if __name__ == "__main__":
+    links = openFile()
+    main(links)
 
-r = searchinPage("/spells/")
-r.html.render(scrolldown=420)
-spellList = r.html.find("#list", first=True)
-spellBlocks = spellList.find(".cards_list__block")
-
-for block in spellBlocks:
-    aLinks = list(block.absolute_links)
-    syncSearch(aLinks)
-    break
-
-print(spellData["4"])
+    # thread2.start()
+    # thread3.start()
