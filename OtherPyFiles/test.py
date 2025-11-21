dab6ef8b3f39c80643218f49489fd5affdaec844
Lokki13579 <l42@lokki13579.ru>
2025-11-21 19:48:17 +0300

    added DB for backgrounds, trinkets, alignments

--- OtherPyFiles/test.py
+++ OtherPyFiles/test.py
@@ -1,6 +1,5 @@
 import json
 import sqlite3
-from typing import Dict, Any
 
 
 def create_database_schema(conn):
@@ -8,97 +7,46 @@ def create_database_schema(conn):
 
     # Таблица классов
     conn.execute("""
-        CREATE TABLE IF NOT EXISTS races (
-            race_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            race_name TEXT UNIQUE NOT NULL,
-            speed TEXT NOT NULL
-        )
-    """)
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS characteristics (
-            char_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            char_name TEXT UNIQUE NOT NULL
-        )
-    """)
-
-    # Таблица уровней классов
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS race_addictions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            race_id INTEGER,
-            char_id INTEGER,
-            increase INTEGER,
-            FOREIGN KEY (race_id) REFERENCES races (race_id),
-            FOREIGN KEY (char_id) REFERENCES characteristics (char_id)
+        CREATE TABLE IF NOT EXISTS statuses (
+            status_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            status_name TEXT UNIQUE NOT NULL
         )
     """)
 
 
-def insert_races_data(conn, race_name: str, race_data: Dict[str, Any]):
+def insert_races_data(conn, name: str):
     """Вставляет данные одной расы в базу"""
 
     # Вставляем основную расу
     cursor = conn.execute(
-        "INSERT OR IGNORE INTO races (race_name, speed) VALUES (?, ?)",
-        (race_name, race_data["скорость"]),
+        "INSERT OR IGNORE INTO statuses (status_name) VALUES (?)",
+        (name,),
     )
 
-    # Получаем ID расы
-    race_id = cursor.lastrowid
-    if race_id == 0:  # Если раса уже существует
-        race_id = conn.execute(
-            "SELECT race_id FROM races WHERE race_name = ?", (race_name,)
-        ).fetchone()[0]
 
-    CHARS = ["Сила", "Ловкость", "Телосложение", "Интеллект", "Мудрость", "Харизма"]
-    for char in CHARS:
-        cursor = conn.execute(
-            """
-            INSERT OR IGNORE INTO characteristics (char_name) VALUES (?)
-        """,
-            (char,),
-        )
-    for char_str, char_increase in race_data.get("УвеличениеХарактеристик", {}).items():
-        print(char_str)
-        char_id = conn.execute(
-            "SELECT char_id FROM characteristics WHERE char_name = ?", (char_str,)
-        ).fetchone()[0]
-        cursor = conn.execute(
-            """INSERT INTO race_addictions
-               (race_id, char_id, increase)
-               VALUES (?, ?, ?)""",
-            (race_id, char_id, char_increase),
-        )
-
-
-def main():
-    # Загружаем JSON данные
-    with open("./JSONS/dnd_races.json", "r", encoding="utf-8") as f:
+def main(file):
+    with open(f"./JSONS/{file}.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    # Создаем базу данных
-    conn = sqlite3.connect("dataBase/dnd_races.db")
+    conn = sqlite3.connect(f"dataBase/{file}.db")
 
-    # Создаем схему
     create_database_schema(conn)
 
-    # Вставляем данные для каждой расы
-    for race_name, race_data in data.items():
-        print(f"Обрабатываю расу: {race_name}")
-        insert_races_data(conn, race_name, race_data)
+    for name in data:
+        print(f"Обрабатываю: {name}")
+        insert_races_data(conn, name)
 
-    # Сохраняем изменения
     conn.commit()
-    print("База данных успешно создана! Файл: dnd_races.db")
+    print("База данных успешно создана!")
 
-    # Показываем статистику
     stats = conn.execute("""
-        SELECT COUNT(*) as race_count FROM races
+        SELECT COUNT(*) as count FROM statuses
     """).fetchone()
-    print(f"Добавлено рас: {stats[0]}")
+    print(f"Добавлено: {stats[0]}")
 
     conn.close()
 
 
 if __name__ == "__main__":
-    main()
+    main(file="dnd_backgrounds")
+    main(file="dnd_trinkets")
