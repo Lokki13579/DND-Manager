2761b917bd3b0b3436c59ba97ffb159896715e58
Lokki13579 <l42@lokki13579.ru>
2025-11-29 14:55:29 +0300

    now all windows not load at startup it is creating when user press the show widget button

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import sys
 from typing import final
 from OtherPyFiles.server_client import ServerClass, Client
-from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QStackedLayout
 
 from UI.MainMenu import Ui_MainMenu
 from UI.ui_Create import Ui_CreateLobby
@@ -41,65 +41,71 @@ class MainWin(QMainWindow):
         self.MainMenu.QuitButton.clicked.connect(lambda: quit())
         self.stackedW.addWidget(self.MainMenu)
 
+        self.stackedW.setCurrentIndex(0)
+
+    def showMainMenu(self):
+        print(self.stackedW.children())
+        for i in self.stackedW.children():
+            if i == self.MainMenu or type(i) is QStackedLayout:
+                continue
+            self.stackedW.removeWidget(i)
+            i.deleteLater()
+        self.stackedW.setCurrentWidget(self.MainMenu)
+
+    def showCreateMenu(self):
+        self.initCreateMenu()
+        self.stackedW.setCurrentWidget(self.CreateMenu)
+
+    def showJoinMenu(self):
+        self.initJoinMenu()
+        self.stackedW.setCurrentWidget(self.JoinMenu)
+
+    def showCharListMenu(self):
+        self.initCharacterList()
+        self.CharacterListMenu.characterFind()
+        self.stackedW.setCurrentWidget(self.CharacterListMenu)
+
+    def showPlayerListMenu(self):
+        self.initPlayerListMenu()
+        self.stackedW.setCurrentWidget(self.PlayerListMenu)
+
+    def showClientLobby(self):
+        self.initClientLobby()
+        self.stackedW.setCurrentWidget(self.ClientLobby)
+        self.ClientLobby.CharSelect.characterFind()
+
+    def showCharCreateMenu(self):
+        self.initCharCreateMenu()
+        self.stackedW.setCurrentWidget(self.CharCreateMenu)
+
+    def initCreateMenu(self):
         self.CreateMenu = Ui_CreateLobby()
-        self.CreateMenu.HostButton.clicked.connect(
-            lambda: self.startServer(int(self.CreateMenu.PortEnter.text()))
-        )
         self.CreateMenu.BackButton.clicked.connect(self.showMainMenu)
         self.stackedW.addWidget(self.CreateMenu)
 
+    def initJoinMenu(self):
         self.JoinMenu = Ui_JoinToServer()
-        self.JoinMenu.ConnectButton.clicked.connect(
-            lambda: self.connectToServer(
-                self.JoinMenu.AddressEnter.text(), int(self.JoinMenu.PortEnter.text())
-            )
-        )
         self.JoinMenu.BackButton.clicked.connect(self.showMainMenu)
         self.stackedW.addWidget(self.JoinMenu)
 
+    def initClientLobby(self):
         self.ClientLobby = Ui_Lobby(ClientOBJ)
         self.stackedW.addWidget(self.ClientLobby)
 
+    def initCharacterList(self):
         self.CharacterListMenu = Ui_CharsList(ClientOBJ)
         self.CharacterListMenu.createNewButton.clicked.connect(self.showCharCreateMenu)
         self.CharacterListMenu.BackButton.clicked.connect(self.showMainMenu)
         self.stackedW.addWidget(self.CharacterListMenu)
 
+    def initCharCreateMenu(self):
         self.CharCreateMenu = Ui_CreateChar()
         self.CharCreateMenu.BackButton.clicked.connect(self.showCharListMenu)
         self.stackedW.addWidget(self.CharCreateMenu)
 
-        self.PlayerListMenu = Ui_PlayerList(ServerOBJ)
+    def initPlayerListMenu(self):
+        self.PlayerListMenu = Ui_PlayerList(ClientOBJ)
         self.stackedW.addWidget(self.PlayerListMenu)
-
-        self.stackedW.setCurrentIndex(0)
-
-    def showMainMenu(self):
-        self.stackedW.setCurrentWidget(self.MainMenu)
-
-    def showCreateMenu(self):
-        self.stackedW.setCurrentWidget(self.CreateMenu)
-
-    def showJoinMenu(self):
-        self.stackedW.setCurrentWidget(self.JoinMenu)
-
-    def showCharListMenu(self):
-        self.CharacterListMenu.characterFind()
-        self.stackedW.setCurrentWidget(self.CharacterListMenu)
-
-    def showPlayerListMenu(self):
-        self.stackedW.setCurrentWidget(self.PlayerListMenu)
-
-    def showClientLobby(self):
-        self.stackedW.setCurrentWidget(self.ClientLobby)
-        self.ClientLobby.CharSelect.characterFind()
-
-    def showCharCreateMenu(self):
-        self.stackedW.removeWidget(self.CharCreateMenu)
-        self.CharCreateMenu = Ui_CreateChar()
-        self.CharCreateMenu.BackButton.clicked.connect(self.showCharListMenu)
-        self.stackedW.addWidget(self.CharCreateMenu)
-        self.stackedW.setCurrentWidget(self.CharCreateMenu)
 
     def startServer(self, _port):
         if ServerOBJ.startServer(_port):
@@ -123,11 +129,12 @@ def applicationset(_argv):
 
 
 if __name__ == "__main__":
-    app = applicationset(sys.argv)
-    mainwin = MainWin()
-    mainwin.show()
-    ex = app.exec()
-    if ex == 0:
+    try:
+        app = applicationset(sys.argv)
+        mainwin = MainWin()
+        mainwin.show()
+        ex = app.exec()
+    except KeyboardInterrupt:
         ServerOBJ.closeServer()
         ClientOBJ.disconnectFromServer()
-    sys.exit(ex)
+        sys.exit(0)
